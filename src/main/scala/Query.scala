d9483f12e6fb8bc17b86df4c667815d828215400
Thibaut Robert <thibaut.robert@worldline.com>
2016-01-15 19:39:13 +0100

    code simplification

--- src/main/scala/Query.scala
+++ src/main/scala/Query.scala
@@ -15,10 +15,13 @@ object Query {
     }
   }
 
-  object CSV {
-    def unapply(s: String) = Some(s.split(", ").toSeq.map(_.toInt))
+  object CSInts {
+    def unapply(s: String) = Some(s.split(", ").map(_.toInt))
   }
-  case class CSV(values: Seq[Int]) {
+  object CSV {
+    def unapply(s: String) = Some(s.split(", "))
+  }
+  case class CSV(values: Seq[Any]) {
     override def toString = values.mkString(", ")
   }
 
@@ -37,9 +40,9 @@ object Query {
     case ri"what is (\d+)$a multiplied by (\d+)$b"                         ⇒ a * b
     case ri"what is (\d+)$a plus (\d+)$b plus (\d+)$c"                     ⇒ a + b + c
     case ri"what is (\d+)$a to the power of (\d+)$b"                       ⇒ Math.pow(a, b).toLong
-    case r"which of the following numbers are primes: (.*)${ CSV(a) }"     ⇒ CSV(a filter isPrime)
-    case r"which of the following numbers is the largest: (.*)${ CSV(a) }" ⇒ a.max
-    case r"which of the following numbers is both a square and a cube: (.*)${ CSV(a) }" ⇒
+    case r"which of the following numbers are primes: (.*)${ CSInts(a) }"     ⇒ CSV(a filter isPrime)
+    case r"which of the following numbers is the largest: (.*)${ CSInts(a) }" ⇒ a.max
+    case r"which of the following numbers is both a square and a cube: (.*)${ CSInts(a) }" ⇒
       CSV(a.filter(n ⇒ isSquare(n) && isCube(n)))
     case ri"what is the (\d+)$n.. number in the Fibonacci sequence" ⇒ fib(n)
     case "who played James Bond in the film Dr No"                  ⇒ "Sean Connery"
@@ -48,10 +51,11 @@ object Query {
     case "what currency did Spain use before the Euro"              ⇒ "peseta"
     case "which city is the Eiffel tower in"                        ⇒ "Paris"
     case "what is your name"                                        ⇒ "tito"
-    case ri"what is (\d+)$a multiplied by (\d+)$b plus (\d+)$c"     => a * b + c
-    case ri"what is (\d+)$a plus (\d+)$b multiplied by (\d+)$c"     => a + b * c
-    case r"what is the english scrabble score of (.*)$a"            ⇒ scrabble_score(a)
-    case r"""which of the following is an anagram of "(.*)$a": (.*)$b""" => (b.split(", ").filter(isAnagram(a, _))).mkString(", ")
+    case ri"what is (\d+)$a multiplied by (\d+)$b plus (\d+)$c"     ⇒ a * b + c
+    case ri"what is (\d+)$a plus (\d+)$b multiplied by (\d+)$c"     ⇒ a + b * c
+    case r"what is the english scrabble score of (.*)$a"            ⇒ (a map score).sum
+    case r"""which of the following is an anagram of "(.*)$a": (.*)${ CSV(b) }""" ⇒
+      CSV(b filter (_.sorted == a.sorted))
     case s ⇒
       println(s"Query: $s")
       "I don't know"
@@ -59,18 +63,15 @@ object Query {
 
   def isSquare(n: Int) = Math.sqrt(n).isWhole()
   def isCube(n: Int) = Math.cbrt(n).isWhole()
-  def isPrime(n: Int) = (n > 0) && ((2 to n - 1) forall (n % _ != 0))
-  def isAnagram(a: String, b: String): Boolean = inAlphabeticalOrder(a).equals(inAlphabeticalOrder(b))
-
-  def inAlphabeticalOrder(a: String) = a.sortWith((c1, c2) => c1 < c2)
+  def isPrime(n: Int) = (n > 0) && (2 until n forall (n % _ != 0))
 
   val fib: Stream[BigInt] = 0 #:: 1 #:: fib.zip(fib.tail).map(p => p._1 + p._2)
-  val score: Map[String, Int] = Map("a" -> 1, "c" -> 3, "b" -> 3, "e" -> 1, "d" -> 2, "g" -> 2,
-    "f" -> 4, "i" -> 1, "h" -> 4, "k" -> 5, "j" -> 8, "m" -> 3,
-    "l" -> 1, "o" -> 1, "n" -> 1, "q" -> 10, "p" -> 3, "s" -> 1,
-    "r" -> 1, "u" -> 1, "t" -> 1, "w" -> 4, "v" -> 4, "y" -> 4,
-    "x" -> 8, "z" -> 10)
-
-  def scrabble_score(word: String): Int = word.foldLeft(0) { case (acc, c) => acc + score.getOrElse(c.toString(), 0) }
+  val score = Map(
+    //in nachtergaele order
+    'a' -> 1, 'c' -> 3, 'b' -> 3, 'e' -> 1, 'd' -> 2, 'g' -> 2,
+    'f' -> 4, 'i' -> 1, 'h' -> 4, 'k' -> 5, 'j' -> 8, 'm' -> 3,
+    'l' -> 1, 'o' -> 1, 'n' -> 1, 'q' -> 10, 'p' -> 3, 's' -> 1,
+    'r' -> 1, 'u' -> 1, 't' -> 1, 'w' -> 4, 'v' -> 4, 'y' -> 4,
+    'x' -> 8, 'z' -> 10)
 
 }
